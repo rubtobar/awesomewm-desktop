6d3aea51c6bf3c3340846251e2809a028a5b8c13
rubtobar <rntobar41@gmail.com>
2021-12-02 20:46:39 +0100

    Removes awesome icon

--- rc.lua
+++ rc.lua
@@ -82,6 +82,8 @@ awful.layout.layouts = {
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
+--[[
+
 myawesomemenu = {
    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
    { "manual", terminal .. " -e man awesome" },
@@ -97,6 +99,9 @@ mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesom
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
+
+]]--
+
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
